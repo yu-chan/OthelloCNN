187ee52dce42790f313f88ac17eb1c5a86657613
yu-chan <y.ooki0125@outlook.com>
2016-11-10 19:18:31 +0900

    配列をコピーするメソッドの追加

--- src/nn/GameState.java
+++ src/nn/GameState.java
@@ -154,6 +154,15 @@ public class GameState {
 		}
 	}
 	
+	//データをコピー
+	public void copyData(int[][] copy) {
+		for(int i = 0; i < data.length; i++) {
+			for(int j = 0; j < data[i].length; j++) {
+				copy[i][j] = data[i][j];
+			}
+		}
+	}
+	
 	public void releaseData() {
 		data = null;
 	}
