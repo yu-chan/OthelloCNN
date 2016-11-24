d11d986cec04de5bc3a882543df5cc5d7f0d85d9
yu-chan <y.ooki0125@outlook.com>
2016-11-24 18:42:03 +0900

    Convolution.javaはsublimeで書いたので、コンパイルするとエラーが起きるので、とりあえず一旦コミットして保存。その後に修正

--- neural_sample/java_sample/convolution/Convolution.java
+++ neural_sample/java_sample/convolution/Convolution.java
@@ -5,6 +5,12 @@ import javax.imageio.ImageIO;
 
 
 public class Convolution {
+	static final float KEISU = 1.0f / 9.0f;//畳み込み係数
+	//畳み込み行列
+	float[][] cMat = {{KEISU, KEISU, KEISU},
+						{KEISU, KEISU, KEISU}
+						{KEISU, KEISU, KEISU}};
+
 	public static void main(String args[]) {
 		File f = new File("xy_regend.jpg");
 		BufferedImage readImage;
@@ -23,7 +29,9 @@ public class Convolution {
 		boolean writeResult;
 		
 		try {
-			writeImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+			//畳み込みをするために、幅・高さにそれぞれ2足してる
+			writeImage = new BufferedImage(w + 2, h + 2, BufferedImage.TYPE_INT_RGB);
+			// writeImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
 			// writeResult = ImageIO.write(writeImage, "jpg", new File("result.jpg"));
 			// writeResult = ImageIO.write(writeImage, "jpg", readImage);
 		} catch (Exception e) {
@@ -31,19 +39,49 @@ public class Convolution {
 			return;
 		}
 
-		for(int y = 0; y < h; y++) {
-			for(int x = 0; x < w; x++) {
-				int c = readImage.getRGB(x, y);
+		//まず、読み込んだ画像に外回りを追加したものを用意する
+		for(int y = 0; y < h + 2; y++) {
+			for(int x = 0; x < w + 2; x++) {
+				if(y == 0 || y == h + 1 || x == 0 || x == w + 1) {
+					writeImage.setRGB(x, y, 0);
+				} else {
+					int c = readImage.getRGB(x - 1, y - 1);
+					writeImage.setRGB(x - 1, y - 1, c);
+				}
+				/*int c = readImage.getRGB(x, y);
 				int r = 255 - r(c);
 				int g = 255 - g(c);
 				int b = 255 - b(c);
 				int rgb = rgb(r, g, b);
-				writeImage.setRGB(x, y, rgb);
+				writeImage.setRGB(x, y, rgb);*/
+			}
+		}
+
+		//畳み込み
+		for(int y = 1; y < h + 1; y++) {
+			for(int x = 1; x < w + 1; x++) {
+				float wa = readImage.getRGB(x - 1, y - 1) * cMat[0][0] + readImage.getRGB(x, y - 1) * cMat[0][1] + readImage.getRGB(x + 1, y - 1) * cMat[0][2]
+						   readImage.getRGB(x - 1, y    ) * cMat[1][0] + readImage.getRGB(x, y    ) * cMat[1][1] + readImage.getRGB(x + 1, y    ) * cMat[1][2]
+						   readImage.getRGB(x - 1, y + 1) * cMat[2][0] + readImage.getRGB(x, y + 1) * cMat[2][1] + readImage.getRGB(x + 1, y + 1) * cMat[2][2];
+				writeImage.setRGB(x, y, wa);
+			}
+		}
+
+		//処理し終わったものを、元の大きさに戻す
+		BufferedImage writeImage2;
+		try {
+			writeImage2 = nwe BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+		} catch(Exception e) {
+			return;
+		}
+		for(int y = 1; y < h + 1; y++) {
+			for(int x = 1; x < w + 1; x++) {
+				writeImage2.setRGB(x - 1, y - 1, writeImage.getRGB(x, y));
 			}
 		}
 
 		try {
-			writeResult = ImageIO.write(writeImage, "jpg", new File("result.jpg"));
+			writeResult = ImageIO.write(writeImage2, "jpg", new File("result2.jpg"));
 		} catch (Exception e) {
 		}
 
