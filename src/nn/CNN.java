e9f2982daf700375cbf459825a681a6208ceb4ad
yu-chan <y.ooki0125@outlook.com>
2016-12-06 19:12:29 +0900

    CNNクラスに幾つかメソッドを追加。ただし、中身は書いてない

--- src/nn/CNN.java
+++ src/nn/CNN.java
@@ -1,7 +1,41 @@
 package nn;
 
 public class CNN {
+	public int[] input_num, middle_num, output_num;//入力層、中間層、出力層の数
+	public double[] w1, w2;//入-中の重み、中-出の重み
+	
 	public CNN() {
 		
 	}
+	
+	/*畳み込み*/
+	public void convolution() {
+		
+	}
+	
+	/*ニューラルネットワーク*/
+	public void neural_network() {
+		
+	}
+	
+	/*入力値と入-中の重みW1の掛け算の和を求める*/
+	public void input_middle() {
+		
+	}
+
+	/*入力値と中-出の重みW2の掛け算の和を求める*/
+	public void middle_output() {
+		
+	}
+	
+	/*シグモイド関数*/
+	public double sigmoid(double x) {
+		return 1.0 / (1.0 + Math.exp(-x));
+	}
+	
+	/*定石をファイルに書き込む*/
+	
+	/*局面をファイルに書き込む*/
+	
+	/*教師データを与える*/
 }
